8895e65e4f87ca28ac1c68a88917120bfa887578
m-nolan <manolan@uw.edu>
2020-11-02 14:58:43 -0800

    Enh: desktop-side fitting code, batch scripts and performance metrics.

--- train_ecog_lfads_fl0u10_reduced.bat
+++ train_ecog_lfads_fl0u10_reduced.bat
@@ -1,9 +1,9 @@
-python train_model_ecog.py ^
+python train_model_ecog_reduced.py ^
 -m lfads_ecog ^
 -d D:\Users\mickey\Data\datasets\ecog\goose_wireless\gw_250_fl0u10 ^
 -p C:\Users\mickey\aoLab\code\hierarchical_lfads\hyperparameters\ecog\lfads_ecog_3.yaml ^
 -o D:\Users\mickey\Data\models\pyt\lfads ^
 --data_suffix ecog_fl0u10 ^
---max_epochs 1500 ^
---batch_size 1000 ^
+--max_epochs 5000 ^
+--batch_size 8000 ^
 -t
