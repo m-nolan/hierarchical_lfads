41b98416bf9fcda83e6d5fd1f0c1fb8ae172cd6e
manolan-ws5 <manolan@uw.edu>
2021-01-26 11:13:58 -0800

    premerge, add .ipynb_checkpoints to .gitignore

--- train_ecog_lfads_allgpu.sh
+++ train_ecog_lfads_allgpu.sh
@@ -6,9 +6,8 @@ python train_model_ecog_reduced.py \
 -p /home/ws5/manolan/ecog_pred/hierarchical_lfads/hyperparameters/ecog/lfads_ecog_reduced_nofilt_allgpu.yaml \
 -o /home/ws5/manolan/data/models/pyt/lfads \
 --data_suffix ecog \
---multidevice \
---max_epochs 4000 \
---batch_size 1500 \
---seq_len 100 \
+--max_epochs 2000 \
+--batch_size 1000 \
+--seq_len 150 \
 --loss mse \
 -t
