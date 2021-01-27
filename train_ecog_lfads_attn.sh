b222845366e252fdcea77ab5d840ff4b20da4677
manolan-ws5 <manolan@uw.edu>
2021-01-27 11:08:48 -0800

    Add: bash launch script for attention module tests train_ecog_lfads_attn.sh

---
+++ train_ecog_lfads_attn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python train_model_ecog_reduced.py \
+-m lfads_ecog \
+-d /home/ws5/manolan/data/datasets/ecog/goose_wireless/gw_250 \
+-p /home/ws5/manolan/ecog_pred/hierarchical_lfads/hyperparameters/ecog/lfads_ecog_4.yaml \
+-o /home/ws5/manolan/data/models/pyt/lfads \
+--data_suffix ecog \
+--max_epochs 200 \
+--batch_size 1000 \
+--seq_len 50 \
+--loss mse \
+-t \
+--attention
