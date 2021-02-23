fe77a0db1a14dc515093ad22d593ade916223077
manolan-ws5 <manolan@uw.edu>
2021-02-22 23:38:28 -0800

    premerge

--- train_ecog_lfads_attn.sh
+++ train_ecog_lfads_attn.sh
@@ -4,10 +4,11 @@ python train_model_ecog_reduced.py \
 -m lfads_ecog \
 -d /home/ws5/manolan/data/datasets/ecog/goose_wireless/gw_250 \
 -p /home/ws5/manolan/ecog_pred/hierarchical_lfads/hyperparameters/ecog/lfads_ecog_reduced_nofilt_allgpu.yaml \
--o /home/ws5/manolan/data/models/pyt/lfads_hikl \
+-o /home/ws5/manolan/data/models/pyt/lfads_inattn \
 --data_suffix ecog \
---max_epochs 2000 \
---batch_size 1500 \
+--max_epochs 2200 \
+--batch_size 500 \
 --seq_len 50 \
 --loss mse \
 -t \
+--attention
